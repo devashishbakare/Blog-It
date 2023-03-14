ebf1012f495453dcb9710b7d33f71f3756999282
devashish bakare <devashishbakare@gmail.com>
2023-03-14 18:50:24 +0530

    added hook to project to make things simple

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,20 +1,21 @@
 import {useState} from "react";
 import { collection, setDoc, doc } from "firebase/firestore";
+import { useFormInput } from "../hooks";
 import {db} from "../firebase";
 
 
 function  CreatePost() {
-  const [title, setTitle] = useState();
-  const [subTitle, setSubTitle] = useState();
-  const [content, setContent] = useState();
+  const title = useFormInput("");
+  const subTitle = useFormInput("");
+  const content = useFormInput("");
 
     async function handleSubmit(event){
     event.preventDefault();
 
     const newPost = {
-      title,
-      subTitle,
-      content,
+      title : title.value,
+      subTitle : subTitle.value,
+      content : content.value,
       createdAt : new Date()
     };
     
@@ -36,17 +37,17 @@ function  CreatePost() {
             <form onSubmit={handleSubmit}>
               <div className="form-field">
                 <label>Title</label>
-                <input value = {title} onChange = {(e) => setTitle(e.target.value)}  />
+                <input {...title} />
               </div>
 
               <div className="form-field">
                 <label>Sub Title</label>
-                <input value = {subTitle} onChange = {(e) => setSubTitle(e.target.value)}  />
+                <input {...subTitle} />
               </div>
 
               <div className="form-field">
                 <label>Content</label>
-                <textarea value = {content} onChange = {(e) => setContent(e.target.value)}></textarea>
+                <textarea {...content}></textarea>
               </div>
 
               <button className="create-post-btn">Create Post</button>
