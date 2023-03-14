70fdf1aeec83c30798e593ca6a6544c5187b3ae2
devashish bakare <devashishbakare@gmail.com>
2023-03-14 16:54:05 +0530

    added Nav bar and link to items of Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,16 @@
+import {Link} from "react-router-dom";
 function Navbar() {
     return(
-        <div className=""><p>Navbar</p></div>
+        <div className="">
+            <ul id = "nav">
+                <li>
+                    <Link to ="/">Home</Link>
+                </li>
+                <li>
+                    <Link to ="/createPost">Create Post</Link>
+                </li>
+            </ul>
+        </div>
     );
   }
   
